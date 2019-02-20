0a13b61ed486dca4bc756d4dc263e9aea464a621
Ryan Armiger <ryanarmiger@dyn3203-225.wlan.ic.ac.uk>
2019-02-20 01:25:08 +0000

    working average

--- ML/Zulu Model/g8TrainPk.py
+++ ML/Zulu Model/g8TrainPk.py
@@ -1,6 +1,9 @@
 import scipy.io as sio
-from tensorflow import keras
-import tensorflow as tf
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+
 
 mat_contents = sio.loadmat('g8data2.mat')
 mat_data = mat_contents['signalArr']
@@ -19,33 +22,33 @@ print("Train Labels shape:", train_labels.shape)
 test_data = mat_data[train_data_num:]
 test_labels = mat_labels[train_data_num:]
 
-model = keras.Sequential([
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dropout(0.25),
-    keras.layers.Dense(64, activation=tf.nn.relu),
-    keras.layers.Dense(2, activation=tf.nn.softmax),
-])
+train_data = train_data.reshape(178, 50)
+test_data = test_data.reshape(test_data.shape[0], 50)
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
+# Generate dummy data
+#x_train = np.random.random((1000, 20))
+#y_train = np.random.randint(2, size=(1000, 1))
+#x_test = np.random.random((100, 20))
+#y_test = np.random.randint(2, size=(100, 1))
+
+model = Sequential()
+model.add(Dense(64, input_dim=50, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
               metrics=['accuracy'])
 
-early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
-model.fit(train_data,
-          train_labels,
-          epochs=20,
-          validation_split=0.3,
-          callbacks=[early_stop])
-print('Model fitting complete')
+model.fit(train_data, train_labels,
+          nb_epoch=20,
+          batch_size=128)
 
-test_labels = (test_labels + 1) % 2
-test_loss, test_acc = model.evaluate(test_data, test_labels)
-print('Test accuracy:', test_acc)
+score = model.evaluate(test_data, test_labels, batch_size=128)
 
-# model.save("g8mlzulu.h5")
 
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-model.save_weights("model.h5")
-print("Saved model to disk")
+import coremltools
+coreml_model = coremltools.converters.keras.convert(model)
+coreml_model.save('zmodel.mlmodel')
