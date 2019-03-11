d04fd11d9a41fd082b139749212211bbc47a695f
Ryan Armiger <ryanarmiger@dyn3200-180.wlan.ic.ac.uk>
2019-03-11 13:21:01 +0000

    storing Becky's work

--- app/Gravity Rush/Gravity Rush/LeaderboardViewController.swift
+++ app/Gravity Rush/Gravity Rush/LeaderboardViewController.swift
@@ -13,6 +13,9 @@ class LeaderboardViewController: UIViewController, UITableViewDelegate, UITableV
     
 
     @IBOutlet private weak var leaderTable: UITableView!
+    @IBOutlet weak var buttonText: UIButton!
+    @IBOutlet weak var searchFriends: UISearchBar!
+    @IBOutlet weak var navView: UIView!
     var ref: DatabaseReference?
     let thisUser = "myUser"
     var locationState = String()
@@ -22,34 +25,78 @@ class LeaderboardViewController: UIViewController, UITableViewDelegate, UITableV
     var leaderNameSortedData: [String] = []
     var positionData: [Int] = []
     var myLocation = Int()
-    var oldScore = Int()
-    let score = 80
+    let score = 70
+    var steve = 2
+    var topConstraint = NSLayoutConstraint()
     
+    @IBAction func addFriendButton(_ sender: UIButton) {
+        if steve == 3{
+            steve = 2
+            buttonText.setTitle("+", for: .normal)
+            searchFriends.isHidden = true
+            NSLayoutConstraint.deactivate([topConstraint])
+            self.topConstraint = leaderTable.topAnchor.constraint(equalTo: navView.bottomAnchor)
+            NSLayoutConstraint.activate([topConstraint])
+        }
+        else{
+            steve = 3
+            buttonText.setTitle("-", for: .normal)
+            searchFriends.isHidden = false
+            NSLayoutConstraint.deactivate([topConstraint])
+            self.topConstraint = leaderTable.topAnchor.constraint(equalTo: searchFriends.bottomAnchor)
+            NSLayoutConstraint.activate([topConstraint])
+        }
+        self.leaderTable.reloadData()
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return(leaderNameSortedData.count)
+        if steve == 2{
+            return(leaderNameSortedData.count)
+        }
+        else{
+            return 2
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "leaderCell", for: indexPath) as! ViewControllerTableViewCell
-        cell.leaderNameLabel.text = leaderNameSortedData[indexPath.row]
-        cell.leaderScoreLabel.text = leaderScoreSortedData[indexPath.row]
-        if positionData[indexPath.row] == -1 {
-            cell.position.text = "..."
-        } else {
-            cell.position.text = String(positionData[indexPath.row])
-        }
-        if leaderNameSortedData[indexPath.row] == thisUser{
-            cell.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        if steve == 2{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "leaderCell", for: indexPath) as! ViewControllerTableViewCell
+            cell.leaderNameLabel.text = leaderNameSortedData[indexPath.row]
+            cell.leaderScoreLabel.text = leaderScoreSortedData[indexPath.row]
+            if positionData[indexPath.row] == -1 {
+                cell.position.text = "..."
+            }
+            else {
+                cell.position.text = String(positionData[indexPath.row])
+            }
+            if leaderNameSortedData[indexPath.row] == thisUser{
+                cell.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+            }
+            else{
+                cell.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            }
+            return(cell)
         }
         else{
-            cell.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            let cell = tableView.dequeueReusableCell(withIdentifier: "addFriendCell", for: indexPath)
+            return(cell)
         }
-        return(cell)
     }
     
     override func viewDidLoad() {
+        
+        //UserDefaults.standard.set(0, forKey: "oldScore")
+        
         super.viewDidLoad()
-        print("before ref")
+        searchFriends.isHidden = true
+        self.topConstraint = leaderTable.topAnchor.constraint(equalTo: navView.bottomAnchor)
+        NSLayoutConstraint.activate([topConstraint])
+        
+        searchFriends.translatesAutoresizingMaskIntoConstraints = false
+        //let topConstraint = leaderTable.topAnchor.constraint(equalTo: navView.bottomAnchor)
+        //NSLayoutConstraint.activate([topConstraint])
+        //searchFriends.translatesAutoresizingMaskIntoConstraints = false
+        
         ref = Database.database().reference()
         var prevData = String()
         var prevDataIndex = 1
@@ -58,9 +105,11 @@ class LeaderboardViewController: UIViewController, UITableViewDelegate, UITableV
             return
         }
         
-        if score > oldScore{
+        let old = UserDefaults.standard.object(forKey: "oldScore") as? Int
+        
+        if score > old!{
             db.child("gameUsers/id18").updateChildValues(["HighScore": score])
-            oldScore = score
+            UserDefaults.standard.set(score, forKey: "oldScore")
         }
         
         let test = db.child("gameUsers").queryOrdered(byChild: "HighScore")
@@ -85,21 +134,29 @@ class LeaderboardViewController: UIViewController, UITableViewDelegate, UITableV
             for i in 0...end{
                 if i < 5{
                     if self.leaderNameData[i]==self.thisUser{
-                        self.oldScore = Int(self.leaderScoreData[i]) ?? 0
+                        //self.oldScore = Int(self.leaderScoreData[i]) ?? 0
                         self.locationState = "top5"
                     }
                 }
                 else if i > 4 && i < 10{
                     if self.leaderNameData[i]==self.thisUser {
-                        self.oldScore = Int(self.leaderScoreData[i]) ?? 0
+                        //self.oldScore = Int(self.leaderScoreData[i]) ?? 0
                         self.locationState = "top10"
                     }
                 }
+                else if i > 9 && i < (end-5){
+                    if self.leaderNameData[i]==self.thisUser{
+                        //self.oldScore = Int(self.leaderScoreData[i]) ?? 0
+                        self.locationState = "notTop"
+                        self.myLocation = i
+                    }
+                }
                 else if self.leaderNameData[i]==self.thisUser{
-                    self.oldScore = Int(self.leaderScoreData[i]) ?? 0
-                    self.locationState = "notTop"
+                    //self.oldScore = Int(self.leaderScoreData[i]) ?? 0
+                    self.locationState = "bottom5"
                     self.myLocation = i
                 }
+                
             }
             
             if self.locationState == "top5"||self.locationState == "top10"{
@@ -151,8 +208,52 @@ class LeaderboardViewController: UIViewController, UITableViewDelegate, UITableV
 
                 }
             }
-            
-            
+            else if self.locationState == "bottom5"{
+                
+                for i in 0...4{
+                    self.leaderScoreSortedData.append(self.leaderScoreData[i])
+                    self.leaderNameSortedData.append(self.leaderNameData[i])
+                    if self.leaderScoreData[i] == prevData{
+                        self.positionData.append(prevDataIndex)
+                    }
+                    else{
+                        self.positionData.append(i+1)
+                        prevDataIndex = i+1
+                    }
+                    prevData = self.leaderScoreData[i]
+                }
+                
+                self.leaderNameSortedData.append("...")
+                self.positionData.append(-1)
+                self.leaderScoreSortedData.append("...")
+                
+                for i in (self.myLocation-5)...(self.myLocation-1){
+                    self.leaderScoreSortedData.append(self.leaderScoreData[i])
+                    self.leaderNameSortedData.append(self.leaderNameData[i])
+                    if self.leaderScoreData[i] == prevData{
+                        self.positionData.append(prevDataIndex)
+                    }
+                    else{
+                        self.positionData.append(i+1)
+                        prevDataIndex = i+1
+                    }
+                    prevData = self.leaderScoreData[i]
+                }
+                
+                for i in (self.myLocation)...end{
+                    self.leaderScoreSortedData.append(self.leaderScoreData[i])
+                    self.leaderNameSortedData.append(self.leaderNameData[i])
+                    if self.leaderScoreData[i] == prevData{
+                        self.positionData.append(prevDataIndex)
+                    }
+                    else{
+                        self.positionData.append(i+1)
+                        prevDataIndex = i+1
+                    }
+                    prevData = self.leaderScoreData[i]
+                }
+                
+            }
             
             self.leaderTable.reloadData()
         })
